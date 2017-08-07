c85b847f542f75c9b1acbc324f20053ca4c6cf47
sasipreetha <30656181+sasipreetha@users.noreply.github.com>
2017-08-07 21:36:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/first', function(req,res){
+    res.send('First Try');
+});
+app.get('/second', function(req,res){
+    res.send('Second Try');
+});
+app.get('/third', function(req,res){
+    res.send('Third Try');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
